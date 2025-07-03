3fc2539caade9b635213cb578b64ca6372df7c47
Abdelrahman Saleh <abdelrahmanisaleh19@gmail.com>
2025-07-03 11:00:34 -0600

    authentication, form & btn widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'core/utils/app_routes.dart';
+import 'core/utils/app_theme.dart';
+import 'features/ui/auth/login/login_screen.dart';
+import 'features/ui/auth/register/register_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
+    return ScreenUtilInit(
+      designSize: const Size(430, 932),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          initialRoute: AppRoutes.loginRoute,
+          routes: {
+            AppRoutes.loginRoute: (context) => LoginScreen(),
+            AppRoutes.registerRoute: (context) => RegisterScreen(),
+          },
+          theme: AppTheme.lightTheme,
+        );
+      },
     );
   }
 }
